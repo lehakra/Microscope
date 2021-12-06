b2f2067027303cdaa743d1b67dcdc881f5a133f0
Romil <57846370+lehakra@users.noreply.github.com>
2021-12-06 23:13:17 +0000

    Template project

---
+++ src/Microscope/Microscope.App/Microscope.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microscope.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Microscope.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ src/Microscope/Microscope.App/Microscope.App/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microscope.App.Models;
+using Microscope.App.ViewModels;
+using Microscope.App.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Microscope.App.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
